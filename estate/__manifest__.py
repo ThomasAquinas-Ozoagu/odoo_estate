cbd08d3259257c23e5b7861b4ba417e0c983daf9
ThomasAquinas-Ozoagu <ozoagu.benedict@gmail.com>
2025-08-11 22:01:07 +0100

    Add estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Sales/Real Estate',
+    'description': "Advertise new houses",
+    'depends': [
+        'base',
+           ],
+    'installable': True,
+    'application': True,
+}
